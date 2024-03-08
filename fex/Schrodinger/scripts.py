a7dc81dfb376482ff85ed0b9afa735ce94aa0c1a
Okpara Ifeanyi <iamokpara@gmail.com>
2024-03-08 08:33:18 +0100

    Update scripts.py with subprocess

--- fex/Schrodinger/scripts.py
+++ fex/Schrodinger/scripts.py
@@ -1,10 +1,9 @@
-import os
+import subprocess
 import time
 import random
-
 
 for i in range(2):
     for dim in [6, 12, 18, 24, 30]:
         gpu = 0
-        python controller_cubic_sh_firstdeflation_thenintegral.py --epoch 1000 --bs 10 --greedy 0.1 --gpu '+str(gpu)+' --ckpt Firstdeflat_thenintegral_epoch1k_Dim'+str(dim)+' --tree depth2_sub --random_step 3 --lr 0.002 --dim '+str(dim)+' --base 200000 --left -1 --right 1  --domainbs 2000 --intbs 10000'
+        subprocess.call(['python','/content/Finite-expression-method/fex/Schrodinger/controller_cubic_sh_firstdeflation_thenintegral.py', '--epoch', '1000', '--bs', '10', '--greedy', '0.1', '--gpu', '0', '--ckpt', 'Firstdeflat_thenintegral_epoch1k_Dim'+str(dim), '--tree', 'depth2_sub', '--random_step', '3', '--lr', '0.002','--dim', str(dim), '--base', '200000', '--left', '-1', '--right', '1', '--domainbs', '2000', '--intbs', '10000'])
         time.sleep(100)
